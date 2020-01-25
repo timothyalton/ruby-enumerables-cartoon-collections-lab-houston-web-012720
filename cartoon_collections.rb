afd40eaff892dfa1177c6ed04147950fec048553
Mitchell T Alton <mitchell.alton@live.com>
2020-01-25 18:20:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+ def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name , index|
+   index += 1 
+   puts "#{index} #{name}"
+  end 
+ end
+
+
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |name|
+    name.capitalize + "!" 
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(words)
+  words.each do |words|
+    if words.length > 4
+      return true
+  else
+    return false
+   end
+  end
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
